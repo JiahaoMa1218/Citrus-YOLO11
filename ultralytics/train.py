5f8ef644725d68a16a221bfdc25e18767755c8cf
JiahaoMa1218 <mamangooococacola7@gmail.com>
2026-06-10 09:38:37 +0800

    Update train.py

--- ultralytics/train.py
+++ ultralytics/train.py
@@ -3,10 +3,8 @@ warnings.filterwarnings('ignore')
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    # 您的自定义网络结构文件
     model_name = 'citrus_yolo11.yaml'  
     
-    # 指向您刚修改的 orange.yaml 的绝对路径
     data_yaml = '/root/autodl-tmp/ultralytics/orange.yaml'
     
     batch_size = 24      
@@ -19,11 +17,9 @@ if __name__ == '__main__':
     print(f"🚀 Starting training for custom model: {model_name}")
     print(f"⚙️  Config: Batch={batch_size} | Epochs={total_epochs} | Img_size={img_size}")
     
-    # 初始化模型结构
     model = YOLO(model_name)
     model.info()
 
-    # 开始训练
     model.train(
         data=data_yaml,
         epochs=total_epochs,
